2ceb27de6c1415c9af16019a14b8727f9aeff5ce
KHollobaugh <khollobaugh@gmail.com>
2018-10-08 23:31:19 -0400

    initial commit, first components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
+import NavBar from './Components/Navigation/NavBar';
+import CardBox from './Components/Cards/CardBox'
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+      <NavBar />
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+      
         </header>
+        <CardBox />
       </div>
     );
   }
